f6d03e34ef87901b57b001a52c4cab52a790e76b
Akif Khan <akhan@globalrescue.com>
2021-04-03 17:33:36 +0500

    UI task

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+// const gulp = require('gulp');
+// const sass = require('gulp-sass');
+
+// sass.compiler = require('node-sass');
+
+// gulp.task('sass',function() {
+//     return gulp.src('./sass/*.scss')
+//     .pipe(sass().on('error',sass.logError))
+//     .pipe(gulp.dest('./css'));
+// });
+
+// gulp.task('sass:watch', function() {
+//     return gulp.watch('./sass/*.scss',gulp.series('sass'))
+// })
+
+// gulp.task('default', gulp.series(['sass','sass:watch']));
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var browserSync = require("browser-sync").create();
+var useref = require("gulp-useref");
+var uglify = require("gulp-uglify-es").default;
+var gulpif = require("gulp-if");
+var cssNano = require("gulp-cssnano");
+
+gulp.task("default", ["watch"], function() {
+  console.log("Hello World..");
+});
+
+gulp.task("sass", function() {
+  console.log("sass");
+  return gulp
+    .src("sass/*.scss")
+    .pipe(sass())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("css"))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
+});
+
+gulp.task("watch", ["sass", "useref", "browserSync"], function() {
+  gulp.watch("sass/*.scss", ["sass"]);
+  gulp.watch("*.html", browserSync.reload);
+  gulp.watch("js/*.js", browserSync.reload);
+});
+
+gulp.task("browserSync", function() {
+  browserSync.init({
+    server: {
+      baseDir: "dist"
+    }
+  });
+});
+
+gulp.task("useref", function() {
+  return gulp
+    .src("*.html")
+    .pipe(useref())
+    .pipe(gulpif("*.js", uglify()))
+    .pipe(gulpif("*.css", cssNano()))
+    .pipe(gulp.dest("dist/"));
+});
+
+gulp.task("build", ["watch"], function() {
+  console.log("building files");
+});
